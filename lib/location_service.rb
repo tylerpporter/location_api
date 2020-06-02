11d29d723d9df8b90e1df2f380d2a28b2320e220
Tyler Porter <tylerpporter@gmail.com>
2020-06-02 15:35:19 -0600

    Add dotenv gem

--- lib/location_service.rb
+++ lib/location_service.rb
@@ -1,13 +1,14 @@
 require 'sinatra'
 require 'faraday'
 require 'yaml'
-# CONFIG =  YAML.load_file('application.yml')
+require 'dotenv'
+Dotenv.load('var.env')
 
 class RapidService < Sinatra::Base
   get '/:zip' do
     response = Faraday.get("https://redline-redline-zipcode.p.rapidapi.com/rest/radius.json/#{params['zip']}/10/mile") do |req|
       req.headers['X-RapidAPI-Host'] = 'redline-redline-zipcode.p.rapidapi.com'
-      req.headers['X-RapidAPI-Key'] = ENV['X-RapidAPI-Key']
+      req.headers['X-RapidAPI-Key'] = ENV['X_RAPIDAPI_KEY']
     end
     response.body
   end
