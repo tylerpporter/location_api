8b795520d469d27c82fcf51a18449681a0de3a25
Tyler Porter <tylerpporter@gmail.com>
2020-06-18 13:11:04 -0600

    Create travis.yml

--- test/location_service_test.rb
+++ test/location_service_test.rb
@@ -2,20 +2,20 @@ require_relative 'test_helper'
 require './lib/location_service.rb'
 
 class RapidServiceTest < Minitest::Test
-    include Rack::Test::Methods
+  include Rack::Test::Methods
 
-    def app
-      RapidService
-    end
+  def app
+    RapidService
+  end
 
-    def test_it_returns_zipcode_info
-      resp = get '/80005'
+  def test_it_returns_zipcode_info
+    resp = get '/80005'
 
-      formatted = JSON.parse(resp.body, symbolize_names: true)
-      expected = formatted[:zip_codes].any? {|hsh| hsh[:zip_code] == '80005'}
+    formatted = JSON.parse(resp.body, symbolize_names: true)
+    expected = formatted[:zip_codes].any? {|hsh| hsh[:zip_code] == '80005'}
 
-      assert formatted[:zip_codes].first[:zip_code]
-      assert_equal 'Golden', formatted[:zip_codes].first[:city]
-      assert expected
-    end
+    assert formatted[:zip_codes].first[:zip_code]
+    assert_equal 'Golden', formatted[:zip_codes].first[:city]
+    assert expected
+  end
 end
